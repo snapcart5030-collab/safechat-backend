00b49258fcfe94f1c6ca918794d3b189a5ec02a9
ganesh-sherkar <sherkarg527@gmail.com>
2026-06-06 23:30:49 +0530

    chat home msg

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,96 @@
 const User = require("../models/User");
+const Message = require("../models/Message");
 
-const getUsers = async (
-  req,
-  res
-) => {
+const getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const currentUserId =
+      req.query.currentUserId;
 
-    res.json(users);
+    const users =
+      await User.find({
+        _id: {
+          $ne: currentUserId,
+        },
+      });
+
+    const usersWithLastMessage =
+      await Promise.all(
+        users.map(
+          async (user) => {
+            const lastMessage =
+              await Message.findOne({
+                $or: [
+                  {
+                    senderId:
+                      currentUserId,
+                    receiverId:
+                      user._id,
+                  },
+                  {
+                    senderId:
+                      user._id,
+                    receiverId:
+                      currentUserId,
+                  },
+                ],
+              }).sort({
+                createdAt: -1,
+              });
+
+            return {
+              ...user.toObject(),
+
+              lastMessage:
+                lastMessage?.message ||
+                "",
+
+              lastMessageTime:
+                lastMessage?.createdAt ||
+                null,
+            };
+          }
+        )
+      );
+
+    res.json(
+      usersWithLastMessage
+    );
   } catch (error) {
     res.status(500).json({
-      message: error.message,
+      message:
+        error.message,
     });
   }
 };
 
-const updateProfile = async (req, res) => {
+const updateProfile = async (
+  req,
+  res
+) => {
   try {
-    const { id, name, picture } = req.body;
-
-    const user = await User.findByIdAndUpdate(
+    const {
       id,
-      { name, picture },
-      { new: true }
-    );
+      name,
+      picture,
+    } = req.body;
+
+    const user =
+      await User.findByIdAndUpdate(
+        id,
+        {
+          name,
+          picture,
+        },
+        {
+          new: true,
+        }
+      );
 
     res.json(user);
   } catch (error) {
     res.status(500).json({
-      message: error.message,
+      message:
+        error.message,
     });
   }
 };
