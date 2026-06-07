f5220974a8db8f8fbc02ca4501099d480c5cbb9b
ganesh-sherkar <sherkarg527@gmail.com>
2026-06-07 13:07:50 +0530

    update firebase

--- firebase.js
+++ firebase.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-admin.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
+}); 
 
 module.exports = admin;
