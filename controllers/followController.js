86e4ab42304f84ac45f535b41d922bc9a173b3e3
ganesh-sherkar <sherkarg527@gmail.com>
2026-06-07 23:12:51 +0530

    follow request fix

--- controllers/followController.js
+++ controllers/followController.js
@@ -1,27 +1,17 @@
 const User = require("../models/User");
 
-const sendFollowRequest = async (
-  req,
-  res
-) => {
+const sendFollowRequest = async (req, res) => {
   try {
-    const {
-      senderId,
-      receiverId,
-    } = req.body;
+    const { senderId, receiverId } = req.body;
 
     if (senderId === receiverId) {
       return res.status(400).json({
-        message:
-          "You cannot follow yourself",
+        message: "You cannot follow yourself",
       });
     }
 
-    const sender =
-      await User.findById(senderId);
-
-    const receiver =
-      await User.findById(receiverId);
+    const sender = await User.findById(senderId);
+    const receiver = await User.findById(receiverId);
 
     if (!sender || !receiver) {
       return res.status(404).json({
@@ -29,223 +19,181 @@ const sendFollowRequest = async (
       });
     }
 
-    const alreadyRequested =
-      receiver.followRequests.some(
-        (id) =>
-          id.toString() === senderId
-      );
+    const alreadyRequested = receiver.followRequests.some(
+      (id) => id.toString() === senderId
+    );
 
     if (alreadyRequested) {
       return res.status(400).json({
-        message:
-          "Request already sent",
+        message: "Request already sent",
       });
     }
 
-    const alreadyFollowing =
-      sender.following.some(
-        (id) =>
-          id.toString() === receiverId
-      );
+    const alreadyFollowing = sender.following.some(
+      (id) => id.toString() === receiverId
+    );
 
     if (alreadyFollowing) {
       return res.status(400).json({
-        message:
-          "Already following",
+        message: "Already following",
       });
     }
 
-    receiver.followRequests.push(
-      senderId
-    );
-
+    receiver.followRequests.push(senderId);
     await receiver.save();
 
-    global.io
-      .to(receiverId)
-      .emit(
-        "newFollowRequest",
-        {
-          senderId,
-        }
-      );
-
-
+    global.io.to(receiverId).emit("newFollowRequest", {
+      senderId,
+      senderName: sender.name,
+    });
 
     res.json({
       success: true,
-      message:
-        "Follow request sent",
+      message: "Follow request sent",
     });
   } catch (error) {
     res.status(500).json({
-      message:
-        error.message,
+      message: error.message,
     });
   }
 };
 
-const getFollowRequests =
-  async (req, res) => {
-    try {
-      const { userId } =
-        req.params;
+const getFollowRequests = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate(
+      "followRequests",
+      "_id name email picture"
+    );
+    res.json(user.followRequests);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-      const user =
-        await User.findById(
-          userId
-        ).populate(
-          "followRequests",
-          "_id name email picture"
-        );
+const acceptFollowRequest = async (req, res) => {
+  try {
+    const { currentUserId, requesterId } = req.body;
 
-      res.json(
-        user.followRequests
-      );
-    } catch (error) {
-      res.status(500).json({
-        message:
-          error.message,
+    const currentUser = await User.findById(currentUserId);
+    const requester = await User.findById(requesterId);
+
+    if (!currentUser || !requester) {
+      return res.status(404).json({
+        message: "User not found",
       });
     }
-  };
 
-const acceptFollowRequest =
-  async (req, res) => {
-    try {
-      const {
-        currentUserId,
-        requesterId,
-      } = req.body;
+    // Remove from followRequests
+    currentUser.followRequests = currentUser.followRequests.filter(
+      (id) => id.toString() !== requesterId
+    );
 
-      const currentUser =
-        await User.findById(
-          currentUserId
-        );
+    // Add to followers and following
+    currentUser.followers.push(requesterId);
+    requester.following.push(currentUserId);
 
-      const requester =
-        await User.findById(
-          requesterId
-        );
+    await currentUser.save();
+    await requester.save();
 
-      if (
-        !currentUser ||
-        !requester
-      ) {
-        return res.status(404).json({
-          message:
-            "User not found",
-        });
-      }
+    // Emit to both users
+    global.io.to(requesterId).emit("followAccepted", {
+      acceptedBy: currentUserId,
+      acceptedUser: requesterId,
+    });
 
-      currentUser.followRequests =
-        currentUser.followRequests.filter(
-          (id) =>
-            id.toString() !==
-            requesterId
-        );
+    global.io.to(currentUserId).emit("followAccepted", {
+      acceptedBy: currentUserId,
+      acceptedUser: requesterId,
+    });
 
-      currentUser.followers.push(
-        requesterId
-      );
+    res.json({
+      success: true,
+      message: "Follow request accepted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-      requester.following.push(
-        currentUserId
-      );
+const rejectFollowRequest = async (req, res) => {
+  try {
+    const { currentUserId, requesterId } = req.body;
+    const currentUser = await User.findById(currentUserId);
 
-      await currentUser.save();
-      await requester.save();
+    currentUser.followRequests = currentUser.followRequests.filter(
+      (id) => id.toString() !== requesterId
+    );
 
-      global.io
-        .to(requesterId)
-        .emit(
-          "followAccepted",
-          {
-            currentUserId,
-          }
-        );
+    await currentUser.save();
 
-      global.io
-        .to(currentUserId)
-        .emit(
-          "followAccepted",
-          {
-            requesterId,
-          }
-        );
+    res.json({
+      success: true,
+      message: "Request rejected",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-      res.json({
-        success: true,
-        message:
-          "Follow request accepted",
-      });
-    } catch (error) {
-      res.status(500).json({
-        message:
-          error.message,
-      });
-    }
-  };
+const getAcceptedUsers = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate(
+      "following",
+      "_id name email picture"
+    );
+    res.json(user.following);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-const rejectFollowRequest =
-  async (req, res) => {
-    try {
-      const {
-        currentUserId,
-        requesterId,
-      } = req.body;
+// NEW: Get followers
+const getFollowers = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate(
+      "followers",
+      "_id name email picture"
+    );
+    res.json(user.followers);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-      const currentUser =
-        await User.findById(
-          currentUserId
-        );
+// NEW: Get all connections (both following and followers)
+const getAllConnections = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId)
+      .populate("following", "_id name email picture")
+      .populate("followers", "_id name email picture");
 
-      currentUser.followRequests =
-        currentUser.followRequests.filter(
-          (id) =>
-            id.toString() !==
-            requesterId
-        );
+    // Combine both arrays and remove duplicates
+    const connections = [...user.following, ...user.followers];
+    const uniqueConnections = connections.filter(
+      (user, index, self) => index === self.findIndex((u) => u._id.toString() === user._id.toString())
+    );
 
-      await currentUser.save();
-
-      res.json({
-        success: true,
-        message:
-          "Request rejected",
-      });
-    } catch (error) {
-      res.status(500).json({
-        message:
-          error.message,
-      });
-    }
-  };
-
-const getAcceptedUsers =
-  async (req, res) => {
-    try {
-      const { userId } =
-        req.params;
-
-      const user =
-        await User.findById(
-          userId
-        ).populate(
-          "following",
-          "_id name email picture"
-        );
-
-      res.json(
-        user.following
-      );
-    } catch (error) {
-      res.status(500).json({
-        message:
-          error.message,
-      });
-    }
-  };
+    res.json(uniqueConnections);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   sendFollowRequest,
@@ -253,4 +201,6 @@ module.exports = {
   acceptFollowRequest,
   rejectFollowRequest,
   getAcceptedUsers,
+  getFollowers,
+  getAllConnections,
 };
--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -1,40 +1,34 @@
 const express = require("express");
-
-const router =
-  express.Router();
-
+const router = express.Router();
 const {
   sendFollowRequest,
   getFollowRequests,
   acceptFollowRequest,
   rejectFollowRequest,
   getAcceptedUsers,
-} = require(
-  "../controllers/followController"
-);
+  getFollowers,
+  getAllConnections,
+} = require("../controllers/followController");
 
-router.post(
-  "/send-request",
-  sendFollowRequest
-);
+// Send follow request
+router.post("/send-request", sendFollowRequest);
 
-router.get(
-  "/requests/:userId",
-  getFollowRequests
-);
+// Get pending follow requests for a user
+router.get("/requests/:userId", getFollowRequests);
 
-router.post(
-  "/accept-request",
-  acceptFollowRequest
-);
+// Accept follow request
+router.post("/accept-request", acceptFollowRequest);
 
-router.post(
-  "/reject-request",
-  rejectFollowRequest
-);
-router.get(
-  "/accepted/:userId",
-  getAcceptedUsers
-);
+// Reject follow request
+router.post("/reject-request", rejectFollowRequest);
+
+// Get users that the current user is following
+router.get("/accepted/:userId", getAcceptedUsers);
+
+// Get users following the current user
+router.get("/followers/:userId", getFollowers);
+
+// Get all connections (mutual + following + followers)
+router.get("/connections/:userId", getAllConnections);
 
 module.exports = router;
